6c83ee788e59f525e6f9176fc42d77d9b27d96ec
Rafael Peres <rafael.peres@allya.com.br>
2020-11-17 23:55:18 -0300

    port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Server from './server';
 import Connection from './database/index';
 
-const PORT = process.env.PORT || 3333;
+const { PORT } = process.env;
+
 new Connection();
 
 const server = new Server();
