db02bc5c4b7fa5c8718bf8c22783129f58ecc82d
Rafael Peres <rafael.peres@allya.com.br>
2020-11-18 00:14:25 -0300

    variable

--- src/database/index.ts
+++ src/database/index.ts
@@ -17,11 +17,11 @@ export default class Connection {
     try {
       this.dbconn = new Sequelize({
         dialect: 'postgres',
-        host: process.env.DB_HOST,
+        host: process.env.DB_HOST || 'lallah.db.elephantsql.com',
         port: Number(process.env.DB_PORT) || 5432,
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
+        username: process.env.DB_USERNAME || 'pfdcmjct',
+        password: process.env.DB_PASSWORD || 'MB8d1oaG-t6CpAJH6Jw1N6rKq9f9qdcV',
+        database: process.env.DB_DATABASE || 'pfdcmjct',
         pool: {
           max: 5,
           min: 0,
